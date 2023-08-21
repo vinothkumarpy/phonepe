049a2ef41a30eb4500dbde50175f2083e2acde20
vinothkumarpy <142497118+vinothkumarpy@users.noreply.github.com>
2023-08-21 12:54:35 +0530

    Add files via upload

---
+++ phonepe_plus.py
@@ -0,0 +1,350 @@
+import pandas as pd
+import json
+import os as os
+import mysql.connector as sql
+import streamlit as st
+import plotly.express as px
+import os
+import json
+from streamlit_option_menu import option_menu
+from PIL import Image
+
+
+data=open(r'D:\vs_code\pulse-master\data\aggregated\transaction\country\india\state\andaman-&-nicobar-islands\2018\1.json','r')
+js=json.load(data)
+print(js)
+
+
+
+path=r"D:\vs_code\pulse-master\data\aggregated\transaction\country\india\state"
+Agg_state_list=os.listdir(path)
+Agg_state_list
+
+
+clm={'State':[], 'Year':[],'Quater':[],'Transacion_type':[], 'Transacion_count':[], 'Transacion_amount':[]}
+
+
+for i in Agg_state_list:
+    p_i=path+"/"+i
+    Agg_yr=os.listdir(p_i)
+    
+    for j in Agg_yr:
+        p_j=p_i+"/"+j
+        Agg_yr_list=os.listdir(p_j)
+        
+        for k in Agg_yr_list:
+            p_k=p_j+'/'+k
+            Data=open(p_k,'r')
+            D=json.load(Data)
+            
+            for z in D['data']['transactionData']:
+              Name=z['name']
+              count=z['paymentInstruments'][0]['count']
+              amount=z['paymentInstruments'][0]['amount']
+              clm['Transacion_type'].append(Name)
+              clm['Transacion_count'].append(count)
+              clm['Transacion_amount'].append(amount)
+              clm['State'].append(i)
+              clm['Year'].append(j)
+              clm['Quater'].append(int(k.strip('.json')))
+#Succesfully created a dataframe
+df_Agg_Trans=pd.DataFrame(clm)
+print(df_Agg_Trans)
+
+
+#Agg_User
+path_2=r"D:\vs_code\pulse-master\data\aggregated\user\country\india\state"
+Agg_userstate_list=os.listdir(path_2)
+Agg_userstate_list
+
+clm_2={'State': [], 'Year': [], 'Quarter': [], 'Brands': [], 'User_Count': [], 'User_Percentage': []}
+
+for i in Agg_userstate_list:
+    p_i = path_2 + "/" + i 
+    Agg_yr = os.listdir(p_i)
+
+    for j in Agg_yr:
+        p_j = p_i + "/" +  j 
+        Agg_yr_list = os.listdir(p_j)
+
+        for k in Agg_yr_list:
+            p_k = p_j + "/" + k
+            Data = open(p_k, 'r')
+            B = json.load(Data)
+            
+            try:
+                for l in B["data"]["usersByDevice"]:
+                    brand_name = l["brand"]
+                    count_ = l["count"]
+                    ALL_percentage = l["percentage"]
+                    clm_2["State"].append(i)
+                    clm_2["Year"].append(j)
+                    clm_2["Quarter"].append(int(k.strip('.json')))
+                    clm_2["Brands"].append(brand_name)
+                    clm_2["User_Count"].append(count_)
+                    clm_2["User_Percentage"].append(ALL_percentage*100)
+            except:
+                pass
+
+df_Agg_user = pd.DataFrame(clm_2)
+print(df_Agg_user)
+
+# map trans
+
+path_3=r"D:\vs_code\pulse-master\data\map\transaction\hover\country\india\state"
+map_trans_state_list=os.listdir(path_2)
+map_trans_state_list
+
+
+clm_3 = {'State': [], 'Year': [], 'Quarter': [], 'District': [], 'Transaction_Count': [], 'Transaction_Amount': []}
+
+for i in map_trans_state_list:
+    p_i = path_3 + "/" + i  
+    Agg_yr = os.listdir(p_i)
+
+    for j in Agg_yr:
+        p_j = p_i + "/" + j 
+        Agg_yr_list = os.listdir(p_j)
+
+        for k in Agg_yr_list:
+            p_k = p_j + "/"+ k
+            Data = open(p_k, 'r')
+            C = json.load(Data)
+            
+            for l in C["data"]["hoverDataList"]:
+                District = l["name"]
+                count = l["metric"][0]["count"]
+                amount = l["metric"][0]["amount"]
+                clm_3['State'].append(i)
+                clm_3['Year'].append(j)
+                clm_3['Quarter'].append(int(k.strip('.json')))
+                clm_3["District"].append(District)
+                clm_3["Transaction_Count"].append(count)
+                clm_3["Transaction_Amount"].append(amount)
+                
+df_map_trans = pd.DataFrame(clm_3)
+
+print(df_map_trans)
+
+#map_user
+path_4=r"D:\vs_code\pulse-master\data\map\user\hover\country\india\state"
+map_user_state_list=os.listdir(path_4)
+map_user_state_list
+
+clm_4= {"State": [], "Year": [], "Quarter": [], "District": [], "Registered_User": []}
+
+for i in map_user_state_list:
+    p_i = path_4 + "/" +i 
+    Agg_yr = os.listdir(p_i)
+
+    for j in Agg_yr:
+        p_j = p_i + "/" + j
+        Agg_yr_list = os.listdir(p_j)
+
+        for k in Agg_yr_list:
+            p_k = p_j + "/" + k
+            Data = open(p_k, 'r')
+            D = json.load(Data)
+
+            for l in D["data"]["hoverData"].items():
+                district = l[0]
+                registereduser = l[1]["registeredUsers"]
+                clm_4['State'].append(i)
+                clm_4['Year'].append(j)
+                clm_4['Quarter'].append(int(k.strip('.json')))
+                clm_4["District"].append(district)
+                clm_4["Registered_User"].append(registereduser)
+                
+df_map_user = pd.DataFrame(clm_4)
+
+print(df_map_user)
+
+
+#top trans
+
+path_5=r"D:\vs_code\pulse-master\data\top\transaction\country\india\state"
+top_trans_state_list=os.listdir(path_5)
+top_trans_state_list
+
+clm_5= {'State': [], 'Year': [], 'Quarter': [], 'District_Pincode': [], 'Transaction_count': [], 'Transaction_amount': []}
+
+for i in top_trans_state_list:
+    p_i = path_5 + "/" + i 
+    Agg_yr = os.listdir(p_i)
+
+    for j in Agg_yr:
+        p_j = p_i + "/" +j
+        Agg_yr_list = os.listdir(p_j)
+
+        for k in Agg_yr_list:
+            p_k = p_j + "/" + k
+            Data = open(p_k, 'r')
+            E = json.load(Data)
+            
+            for l in E['data']['pincodes']:
+                Name = l['entityName']
+                count = l['metric']['count']
+                amount = l['metric']['amount']
+                clm_5['State'].append(i)
+                clm_5['Year'].append(j)
+                clm_5['Quarter'].append(int(k.strip('.json')))
+                clm_5['District_Pincode'].append(Name)
+                clm_5['Transaction_count'].append(count)
+                clm_5['Transaction_amount'].append(amount)
+
+df_top_trans = pd.DataFrame(clm_5)
+
+df_top_trans
+
+#top user
+path_6=r'D:\vs_code\pulse-master\data\top\user\country\india\state'
+top_user_state_list=os.listdir(path_6)
+top_user_state_list
+
+clm_6 = {'State': [], 'Year': [], 'Quarter': [], 'District_Pincode': [], 'Registered_User': []}
+
+for i in top_user_state_list:
+    p_i = path_6 + "/" + i
+    Agg_yr = os.listdir(p_i)
+
+    for j in Agg_yr:
+        p_j = p_i + "/" + j
+        Agg_yr_list = os.listdir(p_j)
+
+        for k in Agg_yr_list:
+            p_k = p_j + "/" +  k
+            Data = open(p_k, 'r')
+            F = json.load(Data)
+            
+            for l in F['data']['pincodes']:
+                Name = l['name']
+                registeredUser = l['registeredUsers']
+                clm_6['State'].append(i)
+                clm_6['Year'].append(j)
+                clm_6['Quarter'].append(int(k.strip('.json')))
+                clm_6['District_Pincode'].append(Name)
+                clm_6['Registered_User'].append(registeredUser)
+                
+df_top_user = pd.DataFrame(clm_6)
+df_top_user
+
+#--------------------------->>>>>>>>>>>>>>------------------------------->>>>>>>>>>>>>>
+
+#drop dublicates
+d1 = df_Agg_Trans.drop_duplicates()
+d2 = df_Agg_user.drop_duplicates()
+d3 = df_map_trans.drop_duplicates()
+d4 = df_map_user.drop_duplicates()
+d5 = df_top_trans.drop_duplicates()
+d6 = df_top_user.drop_duplicates()
+#______________________________________________________________________________________
+
+#checking Null values
+
+null_counts = d1.isnull().sum()
+print(null_counts)
+
+null_counts = d2.isnull().sum()
+print(null_counts)
+
+null_counts = d3.isnull().sum()
+print(null_counts)
+
+null_counts = d4.isnull().sum()
+print(null_counts)
+
+null_counts = d5.isnull().sum()
+print(null_counts)
+
+null_counts = d6.isnull().sum()
+print(null_counts)
+#______________________________________________________________________________________________
+
+# connect sql server
+
+import mysql.connector
+import sqlalchemy
+from sqlalchemy import create_engine
+
+# Connect to the MySQL server
+mydb = mysql.connector.connect(
+  host = "localhost",
+  user = "root",
+  password = "vino8799",
+  database = "phonepe_pulse__"
+)
+
+# Create a new database and use
+mycursor = mydb.cursor()
+mycursor.execute("CREATE DATABASE IF NOT EXISTS phonepe_pulse__")
+
+# Close the cursor and database connection
+mycursor.close()
+mydb.close()
+
+
+engine = create_engine('mysql+mysqlconnector://root:vino8799@localhost/phonepe_pulse__', echo=False)
+
+#insert table one into sql 
+df_Agg_Trans.to_sql('aggregated_transaction', engine, if_exists = 'replace', index=False,   
+                                 dtype={'State': sqlalchemy.types.VARCHAR(length=50), 
+                                       'Year': sqlalchemy.types.Integer, 
+                                       'Quater': sqlalchemy.types.Integer, 
+                                       'Transaction_type': sqlalchemy.types.VARCHAR(length=50), 
+                                       'Transaction_count': sqlalchemy.types.Integer,
+                                       'Transaction_amount': sqlalchemy.types.FLOAT(precision=5, asdecimal=True)})
+
+#inser all table in to sql
+
+# 2
+df_Agg_user.to_sql('aggregated_user', engine, if_exists = 'replace', index=False,
+                          dtype={'State': sqlalchemy.types.VARCHAR(length=50), 
+                                 'Year': sqlalchemy.types.Integer, 
+                                 'Quater': sqlalchemy.types.Integer,
+                                 'Brands': sqlalchemy.types.VARCHAR(length=50), 
+                                 'User_Count': sqlalchemy.types.Integer, 
+                                 'User_Percentage': sqlalchemy.types.FLOAT(precision=5, asdecimal=True)})
+
+# 3                       
+df_map_trans.to_sql('map_transaction', engine, if_exists = 'replace', index=False,
+                          dtype={'State': sqlalchemy.types.VARCHAR(length=50), 
+                                 'Year': sqlalchemy.types.Integer, 
+                                 'Quater': sqlalchemy.types.Integer, 
+                                 'District': sqlalchemy.types.VARCHAR(length=50), 
+                                 'Transaction_Count': sqlalchemy.types.Integer, 
+                                 'Transaction_Amount': sqlalchemy.types.FLOAT(precision=5, asdecimal=True)})
+
+# 4
+df_map_user.to_sql('map_user', engine, if_exists = 'replace', index=False,
+                   dtype={'State': sqlalchemy.types.VARCHAR(length=50), 
+                          'Year': sqlalchemy.types.Integer, 
+                          'Quater': sqlalchemy.types.Integer, 
+                          'District': sqlalchemy.types.VARCHAR(length=50), 
+                          'Registered_User': sqlalchemy.types.Integer, })
+
+# 5                  
+df_top_trans.to_sql('top_transaction', engine, if_exists = 'replace', index=False,
+                         dtype={'State': sqlalchemy.types.VARCHAR(length=50), 
+                                'Year': sqlalchemy.types.Integer, 
+                                'Quater': sqlalchemy.types.Integer,   
+                                'District_Pincode': sqlalchemy.types.Integer,
+                                'Transaction_count': sqlalchemy.types.Integer, 
+                                'Transaction_amount': sqlalchemy.types.FLOAT(precision=5, asdecimal=True)})
+
+# 6
+df_top_user.to_sql('top_user', engine, if_exists = 'replace', index=False,
+                   dtype={'State': sqlalchemy.types.VARCHAR(length=50), 
+                          'Year': sqlalchemy.types.Integer, 
+                          'Quater': sqlalchemy.types.Integer,                           
+                          'District_Pincode': sqlalchemy.types.Integer, 
+                          'Registered_User': sqlalchemy.types.Integer,})
+
+
+
+
+
+
+
+
+
+
---
+++ phonepe_visuallisation.py
@@ -0,0 +1,255 @@
+import mysql.connector
+import pandas as pd
+
+import streamlit as st
+import PIL
+from PIL import Image
+from streamlit_option_menu import option_menu
+import plotly.express as px
+import pandas as pd
+import requests
+
+#connect to the database
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host = "localhost",
+  user = "root",
+  password = "vino8799",
+  database = "phonepe_pulse__"
+)
+
+cursor=mydb.cursor()
+
+SELECT = option_menu(
+    menu_title=None,
+    options=['About' , 'Home' , 'Basic insights' , 'contact'],
+    icons=['bar_chart' , 'house' , 'toggles' , 'at'],
+    default_index=2,
+    orientation='Horizontal',
+    styles={'Container':{'padding': '0!important' , 'background-colour' : 'white' , 'size' : 'cover','width' : '100%' },
+            'icon' : {'colour' : 'black' , 'font-size' : '20px'},
+            'navi-link': {'font-size' : '20px', 'text-align' : 'center' , 'margin' : '-2px' , '--hover-color' : '#6F3AD'},
+            'nav-link-selected':{'background-color':'#6F36AD'}})
+
+
+
+#---------------------Basic Insights -----------------#
+
+
+if SELECT == "Basic insights":
+    st.title("BASIC INSIGHTS")
+    st.write("----")
+    st.subheader("Let's know some basic insights about the data")
+    options = ["--select--",
+               "Top 10 states based on year and amount of transaction",
+               "List 10 states based on type and amount of transaction",
+               "Top 5 Transaction_Type based on Transaction_Amount",
+               "Top 10 Registered-users based on States and District",
+               "Top 10 Districts based on states and Count of transaction",
+               "List 10 Districts based on states and amount of transaction",
+               "List 10 Transaction_Count based on Districts and states",
+               "Top 10 RegisteredUsers based on states and District"]
+    
+               #1
+               
+    select = st.selectbox("Select the option",options)
+    if select=="Top 10 states based on year and amount of transaction":
+        cursor.execute("SELECT DISTINCT State, Year, SUM(Transaction_amount) AS Total_Transaction_Amount FROM top_transaction GROUP BY State, Year ORDER BY Total_Transaction_Amount DESC LIMIT 10");
+        
+        df = pd.DataFrame(cursor.fetchall(), columns=['State','Year', 'Transaction_amount'])
+        col1,col2 = st.columns(2)
+        with col1:
+            st.write(df)
+        with col2:
+            st.title("Top 10 states and amount of transaction")
+            st.bar_chart(data=df,x="Transaction_amount",y="State")
+            
+            #2
+            
+    elif select=="List 10 states based on type and amount of transaction":
+        cursor.execute("SELECT DISTINCT State, SUM(Transaction_count) as Total FROM top_transaction GROUP BY State ORDER BY Total ASC LIMIT 10");
+        df = pd.DataFrame(cursor.fetchall(),columns=['State','Total_Transaction'])
+        col1,col2 = st.columns(2)
+        with col1:
+            st.write(df)
+        with col2:
+            st.title("List 10 states based on type and amount of transaction")
+            st.bar_chart(data=df,x="Total_Transaction",y="State")
+            
+            #3
+            
+    elif select == "Top 5 Transaction_Type based on Transaction_Amount":
+        cursor.execute("SELECT DISTINCT Transacion_type, SUM(Transacion_amount) AS Amount FROM aggregated_transaction GROUP BY Transacion_type ORDER BY Amount DESC LIMIT 5")
+        df = pd.DataFrame(cursor.fetchall(), columns=['Transacion_type', 'Transacion_amount'])
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write(df)
+        with col2:
+            st.title("Top 5 Transaction_Type based on Transaction_Amount")
+            st.bar_chart(data=df, y="Transacion_type", x="Transacion_amount")
+
+            #4
+            
+    elif select=="Top 10 Registered-users based on States and District":
+        cursor.execute("SELECT DISTINCT State, District_Pincode, SUM(Registered_User) AS Users FROM top_user GROUP BY State, District_Pincode ORDER BY Users DESC LIMIT 10");
+        df = pd.DataFrame(cursor.fetchall(),columns=['State','District_Pincode','Registered_User'])
+        col1,col2 = st.columns(2)
+        with col1:
+            st.write(df)
+        with col2:
+            st.title("Top 10 Registered-users based on States and District")
+            st.bar_chart(data=df,y="State",x="Registered_User")
+            
+            #5
+            
+    elif select=="Top 10 Districts based on states and Count of transaction":
+        cursor.execute("SELECT DISTINCT State,District,SUM(Transaction_Count) AS Counts FROM map_transaction GROUP BY State,District ORDER BY Counts DESC LIMIT 10");
+        df = pd.DataFrame(cursor.fetchall(),columns=['State','District','Transaction_Count'])
+        col1,col2 = st.columns(2)
+        with col1:
+            st.write(df)
+        with col2:
+            st.title("Top 10 Districts based on states and Count of transaction")
+            st.bar_chart(data=df,y="State",x="Transaction_Count")
+            
+            #6
+            
+    elif select=="List 10 Districts based on states and amount of transaction":
+        cursor.execute("SELECT DISTINCT State,year,SUM(Transacion_amount) AS Amount FROM aggregated_transaction GROUP BY State, year ORDER BY Amount ASC LIMIT 10");
+        df = pd.DataFrame(cursor.fetchall(),columns=['State','year','Transacion_amount'])
+        col1,col2 = st.columns(2)
+        with col1:
+            st.write(df)
+        with col2:
+            st.title("Least 10 Districts based on states and amount of transaction")
+            st.bar_chart(data=df,y="State",x="Transacion_amount")
+            
+            #7
+            
+    elif select=="List 10 Transaction_Count based on Districts and states":
+        cursor.execute("SELECT DISTINCT State, District, SUM(Transaction_Count) AS Counts FROM map_transaction GROUP BY State,District ORDER BY Counts ASC LIMIT 10");
+        df = pd.DataFrame(cursor.fetchall(),columns=['State','District','Transaction_Count'])
+        col1,col2 = st.columns(2)
+        with col1:
+            st.write(df)
+        with col2:
+            st.title("List 10 Transaction_Count based on Districts and states")
+            st.bar_chart(data=df,y="State",x="Transaction_Count")
+            
+            #8
+             
+    elif select=="Top 10 RegisteredUsers based on states and District":
+        cursor.execute("SELECT DISTINCT State,District, SUM(Registered_User) AS Users FROM map_user GROUP BY State,District ORDER BY Users DESC LIMIT 10");
+        df = pd.DataFrame(cursor.fetchall(),columns = ['State','District','Registered_User'])
+        col1,col2 = st.columns(2)
+        with col1:
+            st.write(df)
+        with col2:
+            st.title("Top 10 RegisteredUsers based on states and District")
+            st.bar_chart(data=df,y="State",x="Registered_User")
+
+
+#----------------Home----------------------#
+cursor = mydb.cursor()
+
+# execute a SELECT statement
+cursor.execute("SELECT * FROM aggregated_transaction")
+
+# fetch all rows
+rows = cursor.fetchall()
+from streamlit_extras.add_vertical_space import add_vertical_space
+
+if SELECT == "Home":
+    col1,col2, = st.columns(2)
+    col1.image(Image.open(r"D:\vs_code\images.jpg"),width = 300)
+    with col1:
+        st.subheader("PhonePe  is an Indian digital payments and financial technology company headquartered in Bengaluru, Karnataka, India. PhonePe was founded in December 2015, by Sameer Nigam, Rahul Chari and Burzin Engineer. The PhonePe app, based on the Unified Payments Interface (UPI), went live in August 2016. It is owned by Flipkart, a subsidiary of Walmart.")
+        st.download_button("DOWNLOAD THE APP NOW", "https://www.phonepe.com/app-download/")
+    with col2:
+        st.video(r"D:\vs_code\A speedy selfie to the rescue_(720P_HD).mp4")
+        
+
+#----------------About-----------------------#
+
+if SELECT == "About":
+    col1,col2 = st.columns(2)
+    with col1:
+        st.video(r"D:\vs_code\A speedy selfie to the rescue_(720P_HD).mp4")
+    with col2:
+        st.image(Image.open(r"D:\vs_code\images.jpg"),width = 500)
+        st.write("---")
+        st.subheader("The Indian digital payments story has truly captured the world's imagination."
+                 " From the largest towns to the remotest villages, there is a payments revolution being driven by the penetration of mobile phones, mobile internet and states-of-the-art payments infrastructure built as Public Goods championed by the central bank and the government."
+                 " Founded in December 2015, PhonePe has been a strong beneficiary of the API driven digitisation of payments in India. When we started, we were constantly looking for granular and definitive data sources on digital payments in India. "
+                 "PhonePe Pulse is our way of giving back to the digital payments ecosystem.")
+    st.write("---")
+    col1,col2 = st.columns(2)
+    with col1:
+        st.title("THE BEAT OF PHONEPE")
+        st.write("---")
+        st.subheader("Phonepe became a leading digital payments company")
+        st.image(Image.open(r'D:\vs_code\phone_pe.webp'),width = 400)
+        with open(r"D:\vs_code\PhonePe_Pulse_BCG_report.pdf","rb") as f:
+            data = f.read()
+        st.download_button("DOWNLOAD REPORT",data,file_name="annual report.pdf")
+    with col2:
+        st.image(Image.open(r"D:\vs_code\phone_pe.webp"),width = 800)
+
+
+#----------------------Contact---------------#
+
+
+
+
+if SELECT == "Contact":
+    name = "Vinoth Kumar"
+    mail = (f'{"Mail :"}  {"vinoharish8799@gmail.com"}')
+    description = "An Aspiring DATA-SCIENTIST..!"
+    social_media = {
+        "GITHUB": "https://github.com/omkarm0542",
+        "LINKEDIN": "https://www.linkedin.com/in/omkar-maurya/",
+        "INSTAGRAM": "https://www.instagram.com/omkaraksh/",
+        }
+    
+    col1, col2, col3 = st.columns(3)
+    col3.image(Image.open("D:\vs_code\InShot_20230325_182113987.jpg"), width=350)
+    with col2:
+        st.title('Phonepe Pulse data visualisation')
+        st.write("The goal of this project is to extract data from the Phonepe pulse Github repository, transform and clean the data, insert it into a MySQL database, and create a live geo visualization dashboard using Streamlit and Plotly in Python. The dashboard will display the data in an interactive and visually appealing manner, with at least 10 different dropdown options for users to select different facts and figures to display. The solution must be secure, efficient, and user-friendly, providing valuable insights and information about the data in the Phonepe pulse Github repository.")
+        st.write("---")
+        st.subheader(mail)
+    st.write("#")
+    cols = st.columns(len(social_media))
+    for index, (platform, link) in enumerate(social_media.items()):
+        cols[index].write(f"[{platform}]({link})")
+
+
+        # MENU 4 - ABOUT
+if SELECT == "About":
+    col1,col2 = st.columns([3,3],gap="medium")
+    with col1:
+        st.write(" ")
+        st.write(" ")
+        st.markdown("### :violet[About PhonePe Pulse:] ")
+        st.write("##### BENGALURU, India, On Sept. 3, 2021 PhonePe, India's leading fintech platform, announced the launch of PhonePe Pulse, India's first interactive website with data, insights and trends on digital payments in the country. The PhonePe Pulse website showcases more than 2000+ Crore transactions by consumers on an interactive map of India. With  over 45% market share, PhonePe's data is representative of the country's digital payment habits.")
+        
+        st.write("##### The insights on the website and in the report have been drawn from two key sources - the entirety of PhonePe's transaction data combined with merchant and customer interviews. The report is available as a free download on the PhonePe Pulse website and GitHub.")
+        
+        st.markdown("### :violet[About PhonePe:] ")
+        st.write("##### PhonePe is India's leading fintech platform with over 300 million registered users. Using PhonePe, users can send and receive money, recharge mobile, DTH, pay at stores, make utility payments, buy gold and make investments. PhonePe forayed into financial services in 2017 with the launch of Gold providing users with a safe and convenient option to buy 24-karat gold securely on its platform. PhonePe has since launched several Mutual Funds and Insurance products like tax-saving funds, liquid funds, international travel insurance and Corona Care, a dedicated insurance product for the COVID-19 pandemic among others. PhonePe also launched its Switch platform in 2018, and today its customers can place orders on over 600 apps directly from within the PhonePe mobile app. PhonePe is accepted at 20+ million merchant outlets across Bharat")
+        
+        st.write("**:violet[My Project GitHub link]** ⬇️")
+        st.write("https://github.com/IamJafar/Phonepe_Pulse_Data_Visualization")
+        st.write("**:violet[Image and content source]** ⬇️")
+        st.write("https://www.prnewswire.com/in/news-releases/phonepe-launches-the-pulse-of-digital-payments-india-s-first-interactive-geospatial-website-888262738.html")
+        
+    with col2:
+        st.write(" ")
+        st.write(" ")
+        st.write(" ")
+        st.write(" ")
+        
+
+
